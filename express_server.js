eed477f0aea2d7e941428d1186e9900de88d7cbb
Ahmed Abdi <a.abdi11965@gmail.com>
2023-10-04 12:39:04 -0400

    Updated

--- express_server.js
+++ express_server.js
@@ -2,17 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  const templateVars = { id, longURL };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars); // "urls_index" should match your EJS file name without the extension
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.use((req, res, next) => {
+  res.locals.partialHeader = "partials/_header";
+  next();
 });
 
 app.get("/urls.json", (req, res) => {
@@ -22,3 +37,8 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
