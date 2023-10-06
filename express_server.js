2084aea2ff99e5e01eaa05c1e88b07ec03aa8d7e
Ahmed Abdi <a.abdi11965@gmail.com>
2023-10-06 02:24:18 -0400

    Registration Page

--- express_server.js
+++ express_server.js
@@ -106,6 +106,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.use((req, res, next) => {
   res.locals.partialHeader = "partials/_header";
   next();
